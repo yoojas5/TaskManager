98dc16891e4c20a307d59d5e2734e900abbe6e18
Yuthika Mirpuri <mirpuri.yuthika@gmail.com>
2025-12-03 15:38:05 +0000

    Remove AddOpenApi/MapOpenApi for compatible publish

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -12,7 +12,7 @@ app.MapControllers();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
--- TaskManager.Api/obj/Release/net10.0/TaskManager.Api.AssemblyInfo.cs
+++ TaskManager.Api/obj/Release/net10.0/TaskManager.Api.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e5fea85b88bdbbb24627ca168806b2023427cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Api")]
